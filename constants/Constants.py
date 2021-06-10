719cac6a01209daa27702a9d67574cdb78fc42f6
Louis <louismemyself@gmail.com>
2021-06-10 22:56:33 +0200

    id for msg

--- constants/Constants.py
+++ constants/Constants.py
@@ -3,7 +3,7 @@ JOEPIC_CHANNEL_NAME = "profile-pictures"
 JOESUGGEST_CHANNEL_NAME = "💡-ideas-and-feedback"
 JOEREACT_CHANNEL_NAME = "📚-guidelines-and-resources"
 
-GUIDELINES_MSG_ID = (852636768788021288,)
+GUIDELINES_MSG_ID = (852636768788021288, 843668142764589076)
 emoji = None
 NAME_OF_EMOJI = 'joe'
 CHECK = "✅"
@@ -15,14 +15,12 @@ ROLE_FOR_CMD = "Bot Master"
 ROLE_TO_VIEW = "Farmer"
 ERROR_ON_JOEPIC = """How to use joeBot for profile pictures:
 
-1. Choose a HEX color or a RGB color in these formats: '#00FFFF', '00FFFF', '0 255 255' or '0,255,255'
+1. Choose a HEX color or a RGB color in these formats: '#00FFFF', '00FFFF', '0 255 255' or '0,255,255'.
 https://htmlcolorcodes.com/color-picker/
 
-2. Enter this command `!joepic [color]` with your selected color
+2. Enter this command `!joepic [color]` for the color of the jacket and `!joepic [color] [color]` for the color of the jacket and the skin  with your selected color(s).
 
-3. Save image + add as your Discord profile photo
-
-Users with a Joe profile picture will be tagged in a @Joe  role!
+3. Save image + add as your Discord profile photo !
 
 If you want to suggest a feature or anything for joebot, use `!suggest [your suggestion here]`!"""
 
