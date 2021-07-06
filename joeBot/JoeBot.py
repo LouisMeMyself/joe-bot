b5e848038d6e7b218084d10441e419044266832a
Louis <louismemyself@gmail.com>
2021-07-06 17:34:30 +0200

    temp fix for joeticker

--- joeBot/JoeBot.py
+++ joeBot/JoeBot.py
@@ -35,20 +35,20 @@ class JoeBot:
         self.discord_bot.loop.create_task(self.joeTicker())
 
     async def joeTicker(self):
+        # while 1:
+            # try:
+        print("joeTicker is up")
         while 1:
-            try:
-                print("joeTicker is up")
-                while 1:
-                    price = await JoeSubGraph.getJoePrice()
-                    activity = "JOE: ${}".format(round(price, 4))
-                    await self.discord_bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
-                    await asyncio.sleep(60)
-            except ConnectionError:
-                print("Connection error, retrying in 60 seconds...")
-            except:
-                print("Error, Quiting")
-                break
+            price = await JoeSubGraph.getJoePrice()
+            activity = "JOE: ${}".format(round(price, 4))
+            await self.discord_bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
             await asyncio.sleep(60)
+            # except ConnectionError:
+            #     print("Connection error, retrying in 60 seconds...")
+            # except:
+            #     print("Error, Quiting")
+            #     break
+            # await asyncio.sleep(60)
 
     async def about(self, ctx):
         if ctx.message.channel.id == self.channels.COMMAND_CHANNEL_ID:
